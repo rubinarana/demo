279442772b290076c5e8cb1aa553b00fdde49b3b
rubinarana <rubinaranamgr@gmail.com>
2020-03-29 18:54:13 +0545

    connection_python

---
+++ python.py
@@ -0,0 +1,8 @@
+import mysql.connector
+
+crudyoutube= mysql.connector.connect(host="localhost", user="root",passwd="")
+print("crudyoutube")
+if (crudyoutube):
+	print("sucessful")
+else:
+	print("unsucessful")
